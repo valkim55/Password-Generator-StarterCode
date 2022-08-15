8bc5c4957e906582928c1d0f961f5663a4a6b4fa
valkim55 <vesper5521@gmail.com>
2022-08-15 17:20:32 -0400

    password length and numbers added

--- develop/script.js
+++ develop/script.js
@@ -24,9 +24,11 @@ var pwGenerator = function () {
     window.alert("Welcome to Password Generator!");
 }
 
+
 //Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+/*
 // define a function writePassword to start interaction with a user
 var writePassword = function() {
     var promptGenerate = window.prompt("To create a password say YES, to exit say NO.");
@@ -56,24 +58,54 @@ var writePassword = function() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//writePassword(); */
 
-//writePassword(); 
+// ask a user how long they want their password to be
+
+var getPasswordLength = function() {
+    var passwordLength = "";
+    // set that the password has to be at least 8 and max 128 characters long
+    while (passwordLength >=8 || passwordLength <= 128) {
+        passwordLength = window.prompt("How many character would you like your password to include? Choose a number between 8 and 128.");
+        if (passwordLength < 8 || passwordLength > 128) {
+            window.prompt("Invalid number. Please try again!");
+        } else {
+            console.log("Password length is " + passwordLength);
+            window.alert("Your password length is " + passwordLength);
+            return passwordLength;
+        }
+    }
+}; // end of passwordLength function
+generateBtn.addEventListener("click", getPasswordLength);
 
 
-
-
-
-
-
-     //(see below) is user expected to type something?
+// next, ask the user if they'd like to include numbers
+var getNumbers = function() {
+    var ifNumbers = window.prompt("Would you like to include numbers in your password? Reply YES or NO");
+    if (ifNumbers === "YES" || ifNumbers === "yes") {
+        console.log("user said " + ifNumbers + " to the numbers");
+        window.alert("Your password will contain numbers. Please continue.");
+        return getNumbers();
+    };
+    
+    if (ifNumbers === "NO" || ifNumbers === "no") {
+        console.log("user said " + ifNumbers + " to the numbers");
+        window.alert("Your password will not contain any numbers. Please continue.");
+        return getNumbers();
+    };
+}; // end of getNumbers function
+generateBtn.addEventListener("click", getNumbers); 
 
         // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
 
-  passwordText.value = password;
 
-}
+
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+
+  //passwordText.value = password;
+
+//}
 
         
