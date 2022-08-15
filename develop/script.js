4165cb707a9210f29a15f55e01426b63a8fd18c8
valkim55 <vesper5521@gmail.com>
2022-08-15 15:08:01 -0400

    intro

--- develop/script.js
+++ develop/script.js
@@ -1,34 +1,6 @@
 // Assignment code here
 
-window.alert("Welcome to Password Generator!");
 
-
-var generateOption = function() {
-    var promptGenerate = window.prompt("To create a password say YES, to exit say NO.");
-
-    // if user's reply is NOT yes or no, alert them that they need to reply with only yes or no and return to the prompt question again
-    if (promptGenerate !== "yes" || promptGenerate !== "no" ) {
-        window.alert("You need to provide a valid answer. Please try again!");
-        return generateOption();
-    }
-    
-    // make it case insensitive
-    promptGenerate = promptGenerate.toLowerCase();
-    // if user chooses to exit ask them if they're sure 
-    if (promptGenerate === "no" || promptGenerate === "NO") {
-        var confirmQuit = window.confirm("Are you sure you would like to exit?");
-
-        // if user confirms to quit tell them "See you next time"
-        if (confirmQuit) {
-            window.alert("See you next time!");
-            console.log("user decided to leave");
-            return true;
-        }
-    }
-
-        
-}
-generateOption();
 // if user replies YES go to generatePassword function
 // if user replies NO then break and exit
 
@@ -47,6 +19,45 @@ generateOption();
 
 
 
+//once the page finished loading the alert pops up
+var pwGenerator = function () {
+    window.alert("Welcome to Password Generator!");
+}
+
+//Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// define a function writePassword to start interaction with a user
+var writePassword = function() {
+    var promptGenerate = window.prompt("To create a password say YES, to exit say NO.");
+
+    // if user's response is empty or null, alert them that they need to reply with only yes or no and return to the prompt question again
+    if (promptGenerate === "" || promptGenerate === "null" ) {
+        window.alert("You need to provide a valid answer. Please try again!");
+        return writePassword();
+    }
+
+    // make it case insensitive
+    promptGenerate = promptGenerate.toLowerCase();
+    // if user chooses to exit ask them if they're sure 
+    if (promptGenerate === "no" || promptGenerate === "NO") {
+        var confirmQuit = window.alert("Are you sure you would like to exit?");
+
+        // if user confirms to quit tell them "See you next time"
+        if (confirmQuit) {
+            window.alert("See you next time!");
+            console.log("user decided to leave");
+            return true;
+        }
+    }
+    return false;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+//writePassword(); 
 
 
 
@@ -54,22 +65,15 @@ generateOption();
 
 
 
-
-
-
-
-
-        // Get references to the #generate element
-//var generateBtn = document.querySelector("#generate");
+     //(see below) is user expected to type something?
 
         // Write password to the #password input
-//function writePassword() {
-  //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-  //passwordText.value = password;
+  passwordText.value = password;
 
-//}
+}
 
-        // Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
+        
