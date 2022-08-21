ee8813a1307e4ccadaaaf1b38a8481ab11dd378b
valkim55 <vesper5521@gmail.com>
2022-08-21 17:21:27 -0400

    added random numbers generator

--- develop/script.js
+++ develop/script.js
@@ -1,57 +1,37 @@
 // Assignment code here
 
-
-// if user replies YES go to generatePassword function
-// if user replies NO then break and exit
-
-// inside the generatePassword function use a conditional statement if or switch to ask user which symbols they'd like to include
-
-// password can be >=8 and <= 128 characters long
-// password can have lower or upper case letters
-// password can have numbers
-// password can have special characters
-
-// user has to use at least one character type to generate the password
-
-// after all prompts are answered the password is generated and it matches the selected criteria
-
-// when password is generated it is displayed on the page   
-
+//Get references to the #generate element from HTML
+var generateBtn = document.querySelector("#generate");
 
 //once the page finished loading the alert pops up
 var pwGenerator = function () {
     window.alert("Welcome to Password Generator!");
-
-    //Get references to the #generate element from HTML
-    var generateBtn = document.querySelector("#generate");
-
-    // define a function writePassword to start interaction with a user
-    var writePassword = function() {
+    
+    var getStarted = function() {
         var promptGenerate = window.prompt("To create a password say YES, to exit say NO.");
-
+        //promptGenerate = promptGenerate.toLowerCase();
         // if user's response is empty or null, alert them that they need to reply with only yes or no and return to the prompt question again
-        if (promptGenerate === "" || promptGenerate === null ) {
-            window.alert("You need to provide a valid answer. Please try again!");
-            return writePassword();
-        // make sure to only accept "yes" or "no" for an answer    
-        } else if (promptGenerate != "yes" && promptGenerate != "no") {
-            window.alert("You need to provide a valid answer. Please try again!");
-            return writePassword();
-        }
-        // make it case insensitive
-        promptGenerate = promptGenerate.toLowerCase();
-        // if user chooses to exit ask them if they're sure 
-        if (promptGenerate === "no" || promptGenerate === "NO") {
-            var confirmQuit = window.alert("Are you sure you would like to exit?");
-        }
-        return false;
-    };
-    writePassword();
-    //writePassword();
-    //document.getElementById("generate").addEventListener("click", writePassword);
-};
+        // if (promptGenerate === "" || promptGenerate === null ) {
+        //     window.alert("You need to provide a valid answer. Please try again!");
+        //     return pwGenerator();
+        // // make sure to only accept "yes" or "no" for an answer    
+        // } 
+        if (promptGenerate === "yes") {
+            window.alert("Press button to start!");
+            console.log("user said yes");
+            getPasswordLength;
+        } else if (promptGenerate === "no") {
+            alert("see you next time!");
+            nowQuit();
+        } else {
+            alert("Invalid. Please try again!");
+            pwGenerator();
+        };
+    }; getStarted(); 
+}; 
 
- 
+
+
 
     // ask a user how long they want their password to be
 var getPasswordLength = function() {
@@ -63,100 +43,105 @@ var getPasswordLength = function() {
     if (passwordLength >=8 && passwordLength <= 128) {
         console.log("password length is " + passwordLength);
         window.alert("Your password length is " + passwordLength);
-        getLetters();
+        //getLetters();
     } else {
         window.alert("Invalid. Please try again!");
         getPasswordLength();
     } return passwordLength;
 };  // end of passwordLength function
-    document.getElementById("generate").addEventListener("click", getPasswordLength);
+document.getElementById("generate").addEventListener("click", getPasswordLength);
     
 
+/*
 
-
-    // next, ask the use if they would like to include letters
-    var getLetters = function() {
-        var ifLetters = window.prompt("Would you like to include the letters in your password? Reply YES or NO.");
-        // use switch statement to check multiple options
-        switch (ifLetters) {
-            // if the user replies YES, let them know password will include letters and move on to the numbers
-            case "yes":
-            case "YES":
-                console.log("user said " + ifLetters + " to the letters");
-                window.alert("Your password will contain letters. Please continue");
-                getNumbers();
-                return true;
-                break;
+// next, ask the use if they would like to include letters
+var getLetters = function() {
+    var ifLetters = window.prompt("Would you like to include the letters in your password? Reply YES or NO.");
+    // use switch statement to check multiple options
+    switch (ifLetters) {
+        // if the user replies YES, let them know password will include letters and move on to the numbers
+        case "yes":
+        case "YES":
+            console.log("user said " + ifLetters + " to the letters");
+            window.alert("Your password will contain letters. Please continue");
+            getNumber();
+            return true;
+            break;
                 
-            // if the user replies NO, let them know password will NOT include letters and move on to the numbers
-            case "no":
-            case "NO":
-                console.log("user said " + ifLetters + " to the letters");
-                window.alert("Your password will not contain any letters. Please continue");
-                getNumbers();
-                return false;
-                break;
+        // if the user replies NO, let them know password will NOT include letters and move on to the numbers
+        case "no":
+        case "NO":
+            console.log("user said " + ifLetters + " to the letters");
+            window.alert("Your password will not contain any letters. Please continue");
+            getNumber();
+            return false;
+            break;
 
-            // if user provides invalid response take them back to the beginning of the function statement    
-            default:
-                window.alert("You didn't choose a valid option. Try again!");
-                getLetters();
-        } // end of switch statement for letters
+        // if user provides invalid response take them back to the beginning of the function statement    
+        default:
+            window.alert("You didn't choose a valid option. Try again!");
+            getLetters();
+    } // end of switch statement for letters
     
-    }; // end of getletter function
-    //getLetters();
-    //document.getElementById("generate").addEventListener("click", getLetters); 
+}; // end of getletter function
 
 
-    // next, ask the user if they'd like to include numbers
-    var getNumbers = function() {
-        var ifNumbers = window.prompt("Would you like to include numbers in your password? Reply YES or NO.");
-        // use switch statement to check multiple options
-        switch (ifNumbers) {
-            // if the user replies YES, let them know password will include numbers and move on to the special characters
-            case "yes":
-            case "YES":
-                console.log("user said " + ifNumbers + " to the numbers");
-                window.alert("Your password will contain numbers. Please continue");
-                getSpecialChar();
-                return true;
-                break;
+// next, find out if the user would like to include numbers
+var numbersArray = Array.from({length: 999}, () => Math.floor(Math.random() * 999));
+console.log(numbersArray);
 
-            // if the user replies NO, let them know password will NOT include numbers and move on to the special characters
-            case "no":
-            case "NO":
-                console.log("user said " + ifNumbers + " to the numbers");
-                window.alert("Your password will not contain any numbers. Please continue");
-                getSpecialChar();
-                return false;
-                break;
+// run the function to get random number is user replied yes
+var generateRandomNumber = function() {
+    var randomNumber = Math.floor(Math.random() * numbersArray.length);
+    console.log(randomNumber);
+};
 
-            default:
-                window.alert("You didn't choose a valid option. Try again!");
-                getNumbers();
+var ifNumbers;
+var getNumber = function() {
+    var ifNumbers = window.prompt("Would you like to include numbers in your password? Reply YES or NO.");
+    // use switch statement to check multiple options
+    switch (ifNumbers) {
+        // if the user replies YES, let them know password will include numbers, tell generateRandomNumber function to run and move on to the special characters
+        case "yes":
+        case "YES":
+            console.log("user said " + ifNumbers + " to the numbers");
+            window.alert("Your password will contain numbers. Please continue");
+            generateRandomNumber();
+            getSpecialChar();
+            return true;
+            break;
 
-        } // end of switch statement for numbers
+        // if the user replies NO, let them know password will NOT include numbers, return false and and move on to the special characters
+        case "no":
+        case "NO":
+            console.log("user said " + ifNumbers + " to the numbers");
+            window.alert("Your password will not contain any numbers. Please continue");
+            getSpecialChar();
+            return false;
+            break;
+
+        default:
+            window.alert("You didn't choose a valid option. Try again!");
+            getNumber();
+    } // end of switch statement for numbers
+}; // end of getNumbers function
+
+
+// last, find out if the user wants to include any special characters in their password
+var specialCharArray = ["!", "(", ")", "#", "$", "%", "&", "*", "+", "?", "@", "<", ">", "~", ",", "/", ":", ";", "=", "[", "]", "{", "}", "_", "^", "|"];
+console.log(specialCharArray);
     
-    }; // end of getNumbers function
-    //getNumbers();
-    //document.getElementById("generate").addEventListener("click", getNumbers); 
-
-
-
-    var specialCharArray = ["!", "(", ")", "#", "$", "%", "&", "*", "+", "?", "@", "<", ">", "~", ",", "/", ":", ";", "=", "[", "]", "{", "}", "_", "^", "|"];
-    console.log(specialCharArray);
+// run the function to generate a random special character, doesn't run unless called 
+var generateRandomChar = function() {
+    var randomChar = Math.floor(Math.random() * specialCharArray.length); //find out why u multiplate math.random() with specCharArray.length
+    console.log(randomChar, specialCharArray[randomChar]);
+};
     
-    // function to generate a random special character, doesn't run unless called 
-        var generateRandomChar = function() {
-        var randomChar = Math.floor(Math.random() * specialCharArray.length); //find out why u multiplate math.random() with specCharArray.length
-        console.log(randomChar, specialCharArray[randomChar]);
-        };
-    
-    // checks if user wants a special character
-    var ifSpecialChar;
-    var getSpecialChar = function() {
-      var ifSpecialChar = window.prompt("Would you like to include special characters in your password? Reply YES or NO");
-      switch (ifSpecialChar) {  
+// checks if user wants a special character
+var ifSpecialChar;
+var getSpecialChar = function() {
+    var ifSpecialChar = window.prompt("Would you like to include special characters in your password? Reply YES or NO");
+    switch (ifSpecialChar) {  
         case "yes":
         case "YES":
             console.log("user said " + ifSpecialChar + " to special characters");
@@ -174,25 +159,22 @@ var getPasswordLength = function() {
             window.alert("You didn't choose a valid option. Try again!");
             getSpecialChar();
             return;
-      } // end of switch statement for special characters
+    } // end of switch statement for special characters
+}; // end of getSpecialChar function
     
-    }; // end of getSpecialChar function
-    
-    //document.getElementById("generate").addEventListener("click", getSpecialChar); 
-
-
+  
     
     // function generatePassword() {
         
     // }
     // generateBtn.addEventListener("click", generatePassword); 
         
+*/
 
-    var nowQuit = function() {
-        var quit = console.log("quit now");
-        document.getElementById("password").innerHTML = "poopeepoo";
-    };
-    document.getElementById("generate").addEventListener("click", nowQuit); 
-
-
-    //pwGenerator();
+var nowQuit = function() {
+    var quit = document.querySelector("#password");
+    quit.innerHTML("poopeepoo");
+    console.log("quit now");
+};
+    
+//pwGenerator();
